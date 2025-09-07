9ce68863f2951f5a6735c39f21eaf9db0414cd18
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-07 14:54:07 +0800

    contoh

---
+++ Latihan1.java
@@ -0,0 +1,16 @@
+
+package latihan1;
+
+
+public class Latihan1 {
+
+      public static void main(String[] args) {
+    System.out.println("berantas buta  coding");
+    
+    int angka1 = 12;
+    int angka2 = 6;
+   
+          System.out.println(angka1 / angka2);
+    }
+    
+}
