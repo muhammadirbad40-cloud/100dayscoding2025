f8e1aaaa99b2ba4211667145a6f5b6629948e9bd
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-10 20:06:08 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+    // Double slash digunakan untuk komentar yang hanya satu baris
+    
+    /*
+    kalau satu slash plus bintang 
+    digunakan untuk komentar yang banyak
+    dan menggunakan beberapa baris
+    */
+    }
+    
+}
