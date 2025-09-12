b1ffd5dc90426df0c6c75f6abf8f08da63facaad
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-12 23:22:05 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+       // byte  -> untuk bilangan bulat dengan ukuran 8 bit
+       byte angka = 127;
+       // short  -> untuk bilangan bulat dengan ukuran 16 bit
+       short angka1 = 32767; 
+       // integer -> untuk bilangan bulat dengan ukuran 32 bit
+       int angka2 = 2147483647;
+       // long  -> untuk bilangan bulat dengan ukuran 64 bit
+       long angka3 = 987654321;
+       
+        System.out.println(angka);
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+    }
+    
+}
