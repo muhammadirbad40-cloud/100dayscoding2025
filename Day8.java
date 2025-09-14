b820280a5e57bc378a4065f176c2847afb9b130f
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-15 00:20:16 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,25 @@
+package day8;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+    
+    // untuk tipe data String itu di gunakan untuk beberapa kata/kalimat 
+      String kata1 = "halo";
+      String kata2 = "semuanya";
+    /*
+     untuk penggunaan tipe data string 
+     isinya itu harus di bungkus dengan tanda ""  
+     */ 
+        System.out.println(kata1);
+        System.out.println(kata2);
+    /* 
+     String juga bisa di gabung di dua variable yg berbeda
+     akan tetapi tipe data nya harus sama-sama String
+    */
+      String kalimat = kata1 +" "+ kata2;
+        System.out.println(kalimat);
+    
+    }
+    
+}
