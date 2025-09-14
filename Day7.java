929c83f4eaef13424f40b73e29d8513dae617217
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-14 16:17:06 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,19 @@
+package charbolean;
+
+public class CharBolean {
+
+    public static void main(String[] args) {
+     
+     // boolean untuk menentukan true/false   
+     boolean sehat = true;
+     boolean sakit = false;
+     // char digunakan untuk penggunaan satu karakter/huruf
+     char gender = 'P';
+     
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(gender);
+       
+    }
+    
+}
