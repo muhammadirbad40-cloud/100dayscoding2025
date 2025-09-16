d848f3d7ca4d39f87b124ba8cde3efe291e9b54b
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-16 23:03:45 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+package day9irbad;
+
+public class Day9Irbad {
+
+    public static void main(String[] args) {
+     int umur,tambah,menjadi;
+     umur = 19;
+     tambah = 1;
+     menjadi = umur + tambah;
+        System.out.println(menjadi);
+     
+    }
+    
+}
