24150ef99fcb55f47a23fb347b7f60d8bdbae831
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-17 10:32:27 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,21 @@
+package day10;
+
+import static java.lang.Math.PI;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+        /*
+        final variable biasa di gunakan untuk menentukan isi variable 
+        dan tidak dapat di ganti karna kalau misalkan kita ganti
+        isi variable final tersebut maka akan terjadi error
+        */   
+        final String ucapan = "Halo, Selamat Datang"; 
+        
+        String nama = "muhammad irbad saadilah";
+     
+        System.out.println(ucapan);
+        System.out.println(nama);
+    }
+    
+}
