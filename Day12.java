2120960d9f3836fdc6d257d3c73ae7cdf55f5299
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-19 14:56:59 +0800

    Create Day12.java
    latihan: membuat program biodata sederhana

---
+++ Day12.java
@@ -0,0 +1,38 @@
+package day12;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+       
+        String nama,nim,hobi,kata,kata1,kata2,kata3,kata4;
+       int usia,tanggal,tahun;
+       float tinggi;
+        
+       nama = "muhammad Irbad Saadilah";
+       nim = "D0225523";
+       usia = 19;
+       tinggi = 176.4f;
+       hobi = "Main Game";
+       kata = "Simullu";
+       kata1 = "Majene";
+       kata2 = "Jomblo fi Sabilillah";
+       kata3 = "081543351923";
+       kata4 = "@mhmd_irbd";
+       tanggal = 06;
+       tahun = 2006;
+       
+        System.out.println("BIODATA DIRI");
+        System.out.println("Nama Lengkap \t: " + nama);
+        System.out.println("Umur \t\t: " + usia);
+        System.out.println("NIM \t\t: " + nim);
+        System.out.println("Alamat \t\t: " + kata);
+        System.out.println("TTL \t\t: "+kata1+"\\"+tanggal+" Juni "+ tahun); 
+        System.out.println("Tinggi Badan \t: " + tinggi);
+        System.out.println("Hobi \t\t: " + hobi);
+        System.out.println("Status \t\t: " + kata2);
+        System.out.println("Nomor HP \t: " + kata3);
+        System.out.println("Instagram \t: " + kata4);
+        
+    }
+    
+}
