f40886aeaca3d4e00368d0146cca3c9c5d11f07e
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-25 11:58:19 +0800

    Create Day18.java
    konversi otomatis (dari tipe data kecil ke besar)

---
+++ Day18.java
@@ -0,0 +1,28 @@
+package day18;
+public class Day18 {
+
+    public static void main(String[] args) {
+       
+     // konversi otomatis   
+        byte a = 5;
+        short b = a;
+        
+        System.out.println(b);
+        
+        short c = 10;
+        int d = c;
+        
+        System.out.println(d);
+        
+        int e = 15;
+        long f = e;
+        
+        System.out.println(f);
+        
+        float g = 17.5f;
+        double h = g;
+        
+        System.out.println(h); 
+    }
+    
+}
