c133e503b793f2c68b1f26a2e39f392e1d3aa749
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-09-26 22:18:37 +0800

    Create Day19.java
    Konversi manual/paksa (dari tipe data besar ke kecil)

---
+++ Day19.java
@@ -0,0 +1,28 @@
+package day19;
+public class Day19 {
+
+    public static void main(String[] args) {
+       
+     // konversi manual   
+        short a = 12;
+        byte b = (byte)a;
+        
+        System.out.println(b);
+        
+        int c = 1556;
+        short d = (short)c ;
+        
+        System.out.println(d);
+        
+        long e = 56676;
+        int f = (int)e;
+        
+        System.out.println(f);
+        
+        double g = 655.7;
+        float h = (float)g;
+        
+        System.out.println(h);
+    }
+    
+}
