8f9bf6fdbd1493b3b03a6a283e402ffe57923ada
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-02 21:08:08 +0800

    Create Day25.java
    operator increment decrement

---
+++ Day25.java
@@ -0,0 +1,23 @@
+package day25;
+public class Day25 {
+
+    public static void main(String[] args) {
+        
+        int x = 5;
+        int y = 5;
+
+        System.out.println("=== Increment (penambahan) ===");
+        System.out.println("Nilai awal x = " + x);
+        System.out.println("Pre-increment (++x) = " + (++x)); 
+        System.out.println("\nPost-increment (x++) = " + (x++)); 
+        System.out.println("Nilai akhir x = " + x);
+
+        System.out.println("\n=== Decrement (pengurangan) ===");
+        System.out.println("Nilai awal y = " + y);
+        System.out.println("Pre-decrement (--y) = " + (--y)); 
+        System.out.println("\nPost-decrement (y--) = " + (y--)); 
+        System.out.println("Nilai akhir y = " + y);
+        
+    }
+    
+}
