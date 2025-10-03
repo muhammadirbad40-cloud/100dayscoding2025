46036f08cb96ebb87eff09511a974b31d23384ed
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-03 21:15:43 +0800

    Create Day26.java
    operator pembanding

---
+++ Day26.java
@@ -0,0 +1,17 @@
+package day26;
+public class Day26 {
+
+    public static void main(String[] args) {
+       
+        int a = 10; 
+        int b = 7;
+        
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a < b); // false
+        System.out.println(a > b); // true
+        System.out.println(a >= b); // true
+        System.out.println(a <= b); // false    
+    }
+    
+}
