334f4095776353c6128cff5c96e08e42156634b4
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-04 22:21:37 +0800

    Create Day27.java
    Bilas muka gosok gigi evaluasi

---
+++ Day27.java
@@ -0,0 +1,19 @@
+package rasyy;
+import java.util.Scanner;
+public class Day27 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int a,b;
+        
+        System.out.print("Masukkan a : ");
+        a = in.nextInt();
+        System.out.print("Masukkan b : ");
+        b = in.nextInt();
+        
+        
+        System.out.println("sebelum " + a + " " + b );
+        System.out.println("sesudah " + b + " " + a );
+        
+    }       
+}
