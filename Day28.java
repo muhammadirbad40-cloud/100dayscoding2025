e6809266495cb7e10b64cc01ecc9f868bdbce41a
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-05 21:11:18 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,14 @@
+package day28;
+public class Day28 {
+
+    public static void main(String[] args) {
+      
+        int a = 7;
+        int b = 5;
+        
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        
+    }
+    
+}
