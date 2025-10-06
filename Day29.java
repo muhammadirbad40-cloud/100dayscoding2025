b29132d46db4f3e3b07ed6bf083c80607c763163
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-06 08:17:27 +0800

    Create Day29.java
    operator pembanding pada java

---
+++ Day29.java
@@ -0,0 +1,15 @@
+package day29;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > b); // true
+        System.out.println(a < b); // false
+
+    }
+
+}
