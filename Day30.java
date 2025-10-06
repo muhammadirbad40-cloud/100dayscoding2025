372e9491d53f87e1846a3381b847952a691daff9
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-06 22:52:21 +0800

    Create Day30.java
    penggunaan operator perbandingan

---
+++ Day30.java
@@ -0,0 +1,15 @@
+package day30;
+
+public class Day30 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+
+}
