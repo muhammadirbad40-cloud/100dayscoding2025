8331c718d739109cadb367ce4eb99adef0400fda
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-28 13:45:14 +0800

    Create Day51.java
    perulangan For

---
+++ Day51.java
@@ -0,0 +1,13 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
