6807a3a8a49f3526626387e2bffb8173d7f8abbb
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-29 21:00:46 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,16 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        
+        while (a > 0){
+            System.out.println("hitung mundur "+a);
+            a--;
+        }
+        System.out.println("\nBOOMMM");
+    }
+
+}
