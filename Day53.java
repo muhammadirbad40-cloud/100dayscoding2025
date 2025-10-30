b9c15d37d092593bb79d558b0778630a20155f71
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-10-30 12:12:10 +0800

    Create Day53.java
    keyword break

---
+++ Day53.java
@@ -0,0 +1,21 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            if (i == 5) break;
+            System.out.println(i);
+        }
+
+        int a = 10;
+        while (a > 0) {
+            if(a == 5)break;
+            System.out.println(a);
+            a--;
+        }
+        
+    }
+
+}
