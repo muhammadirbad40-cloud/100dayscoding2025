cab7dde34591b0c21e58a853795c3da77f175515
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-02 21:08:10 +0800

    Create Day56.java

---
+++ Day56.java
@@ -0,0 +1,16 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        
+        do {
+            System.out.println(a);
+            a--;
+        } while (a > 0);
+        
+    }
+
+}
