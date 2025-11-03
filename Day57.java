955c85b9b67d91d9852376a100ad3c62a8ad0f90
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-03 21:36:31 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,16 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Loop i : " + i);
+            for (int j = 1; j <= 2; j++) {
+                System.out.println("  Loop j : " + j);
+            }
+        }
+
+    }
+
+}
