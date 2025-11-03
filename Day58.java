f212afc912c44e82e0153ddd04b04327f6fc5f0c
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-04 03:44:22 +0800

    Create Day58.java

---
+++ Day58.java
@@ -0,0 +1,19 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int i = n; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+    }
+
+}
