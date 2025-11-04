b60a9d17a772849201d7ac8c5b4fe33affec44de
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-05 02:40:01 +0800

    Create Day59.java

---
+++ Day59.java
@@ -0,0 +1,29 @@
+package Irbdd;
+
+import java.util.Scanner;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan N : ");
+        int n = in.nextInt();
+
+        System.out.println("Bilangan Ganjil");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Bilangan Genap");
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(" ");
+    }
+
+}
