cc26b2f987898191f9078bc9d0a40713e649f52e
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-10 01:06:18 +0800

    Create Day64.java

---
+++ Day64.java
@@ -0,0 +1,22 @@
+package Irbdd;
+
+import java.util.Scanner;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan M : ");
+        int m = in.nextInt();
+        System.out.print("Masukkan N : ");
+        int n = in.nextInt();
+
+        int total = 1;
+        for (int i = 1; i <= n; i++) {
+            total *= m;
+        }
+        System.out.println(m + " pangkat " + n + " adalah : " + total);
+    }
+
+}
