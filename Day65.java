0139164766c20b50013f6d13228fcee89255e511
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-11 01:13:08 +0800

    Create Day65.java

---
+++ Day65.java
@@ -0,0 +1,20 @@
+package Irbdd;
+
+import java.util.Scanner;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan N : ");
+        int n = in.nextInt();
+
+        int total = 1;
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+        }
+        System.out.println("kelipatan " + n + " adalah : " + total);
+    }
+
+}
