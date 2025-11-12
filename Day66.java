87e0d8149d484aedeb2b6ac371e6a59a13f4b155
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-12 20:57:00 +0800

    Create Day66.java

---
+++ Day66.java
@@ -0,0 +1,16 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        System.out.println("===POLA VERTIKAL===");
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        }
+    }
+
+}
