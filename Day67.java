ca1f863ac65b8ec5ccf073c491c66f4ce9faf131
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-13 21:56:17 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,17 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        System.out.println("===POLA HORIZONTAL===");
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("* ");
+        }
+System.out.println(" ");
+    }
+
+}
