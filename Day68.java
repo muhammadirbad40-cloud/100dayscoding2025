4ef8772eef5e9b4db1b713bf4432cc9d71d06308
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-14 21:24:11 +0800

    Day68.java

---
+++ Day68.java
@@ -0,0 +1,20 @@
+package Irbdd;
+
+import java.util.Scanner;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan sisi : ");
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
