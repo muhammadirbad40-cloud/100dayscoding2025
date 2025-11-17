7ae262f0412b29b01c947116cd6c148290b785f4
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-17 21:43:47 +0800

    Day71.java

--- Day71.java
+++ Day71.java
@@ -12,7 +12,7 @@ public class Irbdd {
 
         for (int i = 1; i <= tinggi; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
