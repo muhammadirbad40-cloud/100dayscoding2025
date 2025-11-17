13c8cbeb171abfe05e82ca25c2ad77f3d331cdf8
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-17 21:40:51 +0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,21 @@
+package Irbdd;
+
+import java.util.Scanner;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Tinggi segitiga : ");
+        int tinggi = in.nextInt();
+
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
