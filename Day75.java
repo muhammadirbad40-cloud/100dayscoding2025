db4d364eda5d1cc8b9cfecb4c94fec6c2aa31950
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-21 20:15:32 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,15 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+        
+        String nama = "Muhammad Irbad Saadilah";
+        
+        //method String dasar
+        System.out.println("Jumlah Karakter\t: "+nama.length());//termasuk spasi
+        System.out.println("Uppercase\t: "+nama.toUpperCase());
+        System.out.println("Lowercase\t: "+nama.toLowerCase());
+
+    }
+}
