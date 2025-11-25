0b5447ddc4df553d4dfe03b8a745ee5e1f5992a6
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-26 03:31:35 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,14 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int[] coba = {10, 20, 30};
+
+        System.out.println(coba[0]);
+        System.out.println(coba[1]);
+        System.out.println(coba[2]);
+
+    }
+}
