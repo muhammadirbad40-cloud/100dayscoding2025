30dcdc1c2b054072fe87fe783c592881dbc6764f
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-28 21:21:54 +0800

    Day82.java

---
+++ Day82.java
@@ -0,0 +1,15 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int[] coba = {10, 20, 30, 40, 50, 60, 70};
+
+        System.out.print("Angka : ");
+        for (int i = 0; i < coba.length; i++) {
+            System.out.print(coba[i] + " ");
+        }
+        System.out.println("");
+    }
+}
