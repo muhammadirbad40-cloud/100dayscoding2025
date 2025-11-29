7893f8c911b80319d5b47d19f206eaf27b2c3bdf
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-11-29 20:59:09 +0800

    Day83.java

---
+++ Day83.java
@@ -0,0 +1,15 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int[] coba = {10, 20, 30, 40, 50, 60, 70};
+
+        int total = 0;
+        for (int i = 0; i < coba.length; i++) {
+            total += coba[i];
+        }
+        System.out.println("Total : "+total);
+    }
+}
