f56b75de2deaaf6884b02018b03c97bdd281c621
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-12-04 22:28:12 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,14 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        int[] nilai = {10, 20, 30, 40, 50};
+
+        for (int i : nilai) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
