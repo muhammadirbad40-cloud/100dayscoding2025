7732f2a11ea39cb06e239f1a140b30bb3bf4782c
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-12-05 21:38:57 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,15 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static void main(String[] args) {
+
+        coba();
+    }
+
+    public static void coba() {
+
+        System.out.println("Halo semua, Nama saya muhammad irbad");
+    }
+
+}
