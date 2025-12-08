77053783136335545645f7dbf95efb1f92b6845e
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-12-08 22:36:01 +0800

    Day92.java

---
+++ Day92.java
@@ -0,0 +1,15 @@
+package Irbdd;
+
+public class Irbdd {
+
+    public static String salam() {
+
+        return "assalamualaikum";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(salam());
+    }
+
+}
