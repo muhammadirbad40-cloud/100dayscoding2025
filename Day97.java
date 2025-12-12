74a7f4be44f08e9bcbea4a4136f308330a16b29c
Mhmd_irbd <muhammadirbad40@gmail.com>
2025-12-13 02:15:30 +0800

    Day97.java

--- Day97.java
+++ Day97.java
@@ -1,19 +1,19 @@
-package soal2;
+package Irbdd;
 
 import java.util.Scanner;
 
-public class Soal2 {
+public class Irbdd {
 
-    public static void persegi(int s) {
+    public static int persegi(int s) {
 
         int L;
-
         L = s * s;
 
         System.out.println("\nLuas = Sisi x Sisi");
         System.out.println("Luas = " + s + " x " + s + " = " + L);
         System.out.println("Luas Persegi = " + L);
-
+        
+        return L;
     }
 
     public static void main(String[] args) {
